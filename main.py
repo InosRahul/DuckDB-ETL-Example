c818700468e4a980b9c990eb20376523dcd39fbd
Rahul Soni <rahulsoni2225@gmail.com>
2023-06-10 01:54:54 +0530

    fix: add logging and error handling

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import duckdb
 from duckdb import DuckDBPyConnection
 import os
+import logging
 
-create_table_vehicles = """
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+CREATE_TABLE_VEHICLES = """
         DROP TABLE IF EXISTS vehicles CASCADE;
         CREATE TABLE vehicles (
             VIN VARCHAR(10),
@@ -25,37 +30,44 @@ create_table_vehicles = """
     );
 """
 
-select_all_vehicles = """
+SELECT_ALL_VEHICLES = """
     SELECT * from vehicles
 """
 
-describle_vehicles = """
+DESCRIBE_VEHICLES = """
     DESCRIBE vehicles
 """
 
 
-def insertData(cursor: DuckDBPyConnection):
-    cursor.sql(
-        "INSERT INTO vehicles SELECT * FROM read_csv_auto('data/Electric_Vehicle_Population_Data.csv');"
-    )
+def insert_data(cursor: DuckDBPyConnection, csv_file_path: str):
+    try:
+        cursor.sql(
+            f"INSERT INTO vehicles SELECT * FROM read_csv_auto('{csv_file_path}');"
+        )
+        cursor.commit()
+        logger.info("Data insertion successful.")
+    except Exception as e:
+        logger.error(f"Failed to insert data: {str(e)}")
 
-    cursor.commit()
 
-
-def countCarsPerCity(cursor: DuckDBPyConnection):
+def count_cars_per_city(cursor: DuckDBPyConnection):
     cars_per_city_query = """
     SELECT city, COUNT(VIN) AS cars_in_city 
     FROM vehicles 
     GROUP BY city
     """
 
-    cursor.execute(cars_per_city_query)
-    results = cursor.fetchall()
-    return results
+    try:
+        cursor.execute(cars_per_city_query)
+        results = cursor.fetchall()
+        return results
+    except Exception as e:
+        logger.error(f"Failed to count cars per city: {str(e)}")
+        return []
 
 
-def topThreePopluarVehicles(cursor: DuckDBPyConnection):
-    top_three_popular_vehicles = """
+def find_top_three_popular_vehicles(cursor: DuckDBPyConnection):
+    top_three_popular_vehicles_query = """
     SELECT CONCAT(make, ' ', model), 
     COUNT(VIN) as vehicle_count 
     FROM vehicles 
@@ -64,13 +76,17 @@ def topThreePopluarVehicles(cursor: DuckDBPyConnection):
     LIMIT 3
     """
 
-    cursor.execute(top_three_popular_vehicles)
-    results = cursor.fetchall()
-    return results
+    try:
+        cursor.execute(top_three_popular_vehicles_query)
+        results = cursor.fetchall()
+        return results
+    except Exception as e:
+        logger.error(f"Failed to find top three popular vehicles: {str(e)}")
+        return []
 
 
-def mostPopularVehicleByPostalCode(cursor: DuckDBPyConnection):
-    most_popular_vehicle_by_postal_code = """
+def find_most_popular_vehicle_by_postal_code(cursor: DuckDBPyConnection):
+    most_popular_vehicle_by_postal_code_query = """
     SELECT CONCAT(t1.make, ' ', t1.model),
     t1.postal_code
     FROM (
@@ -82,22 +98,29 @@ def mostPopularVehicleByPostalCode(cursor: DuckDBPyConnection):
     WHERE t1.rank = 1
     """
 
-    cursor.execute(most_popular_vehicle_by_postal_code)
-    results = cursor.fetchall()
-    return results
+    try:
+        cursor.execute(most_popular_vehicle_by_postal_code_query)
+        results = cursor.fetchall()
+        return results
+    except Exception as e:
+        logger.error(f"Failed to find most popular vehicle by postal code: {str(e)}")
+        return []
 
 
-def numCarsByModelYear(cursor: DuckDBPyConnection):
-    num_cars_by_model_year = """
+def count_cars_by_model_year(cursor: DuckDBPyConnection):
+    count_cars_by_model_year_query = """
     SELECT model_year, COUNT(vin) as num_cars_year
     FROM vehicles
     GROUP BY model_year
     """
 
-    cursor.execute(num_cars_by_model_year)
-    rows = cursor.fetchall()
-
-    return rows
+    try:
+        cursor.execute(count_cars_by_model_year_query)
+        rows = cursor.fetchall()
+        return rows
+    except Exception as e:
+        logger.error(f"Failed to count cars by model year: {str(e)}")
+        return []
 
 
 def main():
@@ -105,38 +128,48 @@ def main():
 
     cursor = conn.cursor()
 
-    cursor.execute(create_table_vehicles)
+    # Create vehicles table
+    try:
+        cursor.execute(CREATE_TABLE_VEHICLES)
+        logger.info("Vehicles table created.")
+    except Exception as e:
+        logger.error(f"Failed to create vehicles table: {str(e)}")
+        conn.close()
+        return
 
-    insertData(cursor)
+    # Insert data into the table
+    csv_file_path = 'data/Electric_Vehicle_Population_Data.csv'
+    insert_data(cursor, csv_file_path)
 
-    cars_per_city = countCarsPerCity(cursor)
+    # Perform data analysis
+    cars_per_city = count_cars_per_city(cursor)
+    logger.info(f"Cars per city: {cars_per_city}")
 
-    print(cars_per_city)
+    top_three_popular_vehicles = find_top_three_popular_vehicles(cursor)
+    logger.info(f"Top three popular vehicles: {top_three_popular_vehicles}")
 
-    top_three_popular_vehicles = topThreePopluarVehicles(cursor)
+    most_popular_vehicle_by_postal_code = find_most_popular_vehicle_by_postal_code(cursor)
+    logger.info(f"Most popular vehicle by postal code: {most_popular_vehicle_by_postal_code}")
 
-    print(top_three_popular_vehicles)
-
-    most_popular_vehicle_by_postal_code = mostPopularVehicleByPostalCode(cursor)
-
-    print(most_popular_vehicle_by_postal_code)
-
-    rows = numCarsByModelYear(cursor)
+    rows = count_cars_by_model_year(cursor)
 
     output_dir = "data/results"
     os.makedirs(output_dir, exist_ok=True)
 
     for row in rows:
-        year = row[0]
-        count = row[1]
+        year, count = row
 
         year_dir = os.path.join(output_dir, str(year))
         os.makedirs(year_dir, exist_ok=True)
 
         file_path = os.path.join(year_dir, f"{year}.parquet")
 
-        with open(file_path, "w") as file:
-            file.write(f"Count: {count}")
+        try:
+            with open(file_path, "w") as file:
+                file.write(f"Count: {count}")
+            logger.info(f"File saved: {file_path}")
+        except Exception as e:
+            logger.error(f"Failed to save file: {str(e)}")
 
     conn.close()
 
